d899492bfd8e19bbe6d25ef348177dc5272e46b8
Daffie95 <patrik.svahnstrom@gmail.com>
2021-04-06 14:45:37 +0000

    forgot slash

--- products/urls.py
+++ products/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.all_products, name='products'),
-    path('<int:product_id>', views.detailed_product, name='detailed_product'),
+    path('<int:product_id>/', views.detailed_product, name='detailed_product'),
     path('add/', views.add_product, name='add_product'),
 ]
